fea26ee749202b4c7476a07594392cc700b0cbea
David Ben-Yaakov <david@tangogolfdigital.com>
2019-06-30 10:36:28 -0500

    Added more logical operators

--- Sources/LogicalOperators/LogicalOperators.swift
+++ Sources/LogicalOperators/LogicalOperators.swift
@@ -6,37 +6,84 @@ precedencegroup LogicalImplicationPrecedence {
     higherThan: LogicalComparisonPrecedence
 }
 
-prefix operator ¬                               // NEGATION
-infix operator ∧ : LogicalConjunctionPrecedence // CONJUNCTION
-infix operator ∨ : LogicalDisjunctionPrecedence // DISJUNCTION
-infix operator → : LogicalImplicationPrecedence // IMPLICATION
-infix operator ⊕ : LogicalComparisonPrecedence  // EXCLUSIVE DISJUNCTION
-infix operator ≡ : LogicalComparisonPrecedence  // EQUIVALENCE
+prefix operator ¬                               // Negation
+infix operator ∧ : LogicalConjunctionPrecedence // Conjunction
+infix operator ⊼ : LogicalConjunctionPrecedence // Negated conjunction
+infix operator ∨ : LogicalDisjunctionPrecedence // Disjunction
+infix operator ⊽ : LogicalDisjunctionPrecedence // Negated disjunction
+infix operator → : LogicalImplicationPrecedence // Implication
+infix operator ← : LogicalImplicationPrecedence // Converse implication
+infix operator ↔ : LogicalComparisonPrecedence  // Bidirectional implication (Precedence same as comparison because same as 'Equivalence')
+infix operator ⊕ : LogicalComparisonPrecedence  // Exclusive disjunction
+infix operator ⊻ : LogicalComparisonPrecedence  // Exclusive disjunction
+infix operator ⩒ : LogicalComparisonPrecedence  // Exclusive disjunction
+infix operator ≡ : LogicalComparisonPrecedence  // Equivalence
+infix operator ≢ : LogicalComparisonPrecedence  // Not equivalent
 
 extension Bool {
+    // Negation
     public static prefix func ¬ (operand:Bool) -> Bool {
         return !operand
     }
 
+    // Conjunction
     public static func ∧ (left:Bool, right:Bool) -> Bool {
         return left && right
     }
 
+    // Negated conjunction
+    public static func ⊼ (left:Bool, right:Bool) -> Bool {
+        return !(left && right)
+    }
+
+    // Disjunction
     public static func ∨ (left:Bool, right:Bool) -> Bool {
         return left || right
     }
 
+    // Negated disjunction
+    public static func ⊽ (left:Bool, right:Bool) -> Bool {
+        return !(left || right)
+    }
+
+    // Implication
     public static func → (left:Bool, right:Bool) -> Bool {
         return !left || right
+    }
+
+    // Converse implication
+    public static func ← (left:Bool, right:Bool) -> Bool {
+        return left || !right
+    }
+
+    // Bidirectional implication
+    public static func ↔ (left:Bool, right:Bool) -> Bool {
+        return left == right
+    }
+
+    // Exclusive disjunction
+    public static func ⊕ (left:Bool, right:Bool) -> Bool {
+        return left != right
+    }
+
+    // Exclusive disjunction
+    public static func ⊻ (left:Bool, right:Bool) -> Bool {
+        return left != right
+    }
+
+    // Exclusive disjunction
+    public static func ⩒ (left:Bool, right:Bool) -> Bool {
+        return left != right
     }
 
     public static func ≡ (left:Bool, right:Bool) -> Bool {
         return left == right
     }
 
-    public static func ⊕ (left:Bool, right:Bool) -> Bool {
-        return !(left == right)
+    public static func ≢ (left:Bool, right:Bool) -> Bool {
+        return left != right
     }
+
 
 }
 
